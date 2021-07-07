ba1738aaeeab3368675f0329e8c882c9ef9dcdc9
Faker-14 <57102573+Faker-14@users.noreply.github.com>
2021-07-06 20:41:52 -0400

    Day1.1

--- Day1.py
+++ Day1.py
@@ -1,6 +1,4 @@
-message = 'Welcome to my band name geneartor.'
-print(message)
+print('Welcome to my Band Name Geneartor.')
 city_name = input('Please enter the city that you grew up in: \n')
 pet_name = input('Please enter your pet\'s name: \n')
-final_message = f"Your band name could be {city_name} {pet_name}"
-print(final_message)
+print(f"Your band name could be {city_name} {pet_name}")
