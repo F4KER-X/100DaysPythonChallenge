a9906a40d6e934f68cafbe4eaf84fd1d4548e395
Faker-14 <dannymousa1999@hotmail.com>
2021-07-26 09:12:01 -0400

    Day 24

---
+++ Day 024 - Mail Merge/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open(r"C:\Users\danny\Documents\GitHub\100DaysPythonChallenge\Day 024 - Mail Merge\Input\Names\invited_names.txt") as invited:
+    names = invited.readlines()
+
+with open(r"C:\Users\danny\Documents\GitHub\100DaysPythonChallenge\Day 024 - Mail Merge\Input\Letters\starting_letter.docx") as letter:
+    message = letter.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = message.replace(PLACEHOLDER, stripped_name)
+        with open(f"Day 024 - Mail Merge/Output/ReadyToSend/letterTo{stripped_name}.docx", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
